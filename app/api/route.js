156beb592139863de94d8adc67601a35a905e1e0
Guilherme Vieira <gu1lh3rmesv@gmail.com>
2025-07-30 22:40:32 -0300

    create api,  User model with mongoose, save connection string mongodb

--- app/api/route.js
+++ app/api/route.js
@@ -3,3 +3,6 @@ import { NextResponse } from "next/server";
 export async function GET(req) {
     return NextResponse.json({ time: new Date().toLocaleString() })
 }
+
+
+//mongodb+srv://gu1lh3rmesv:<BobEsponja>@cluster0.uanfodt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
