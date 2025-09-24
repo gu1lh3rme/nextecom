34aa6f4ef37287fc17e27cf53997457bce8b639d
DESKTOP-UOSJ1LD\Guilherme <a14012@bcc.unifal-mg.edu.br>
2025-09-24 19:32:54 -0300

    start auth

--- config.js
+++ config.js
@@ -1,5 +1,11 @@
 const DB_URI = "mongodb+srv://gu1lh3rmesv:<BobEsponja>@cluster0.uanfodt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
+const API = process.env.NODE_ENV === "production" ? "https://xxx:vercel.com/api" : "http://localhost:3000/api"
+
+const NEXTAUTH_SECRET = "SUDAFEW0938UR03294UR84390IU"
+
 module.exports = {
     DB_URI,
+    API,
+    NEXTAUTH_SECRET
 }
