661ae8ed65ab44438905bbd7efd538eba274c56a
DESKTOP-UOSJ1LD\Guilherme <a14012@bcc.unifal-mg.edu.br>
2025-09-26 15:55:41 -0300

    auth options

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,1 +1,17 @@
-import {CredentialsProvider} from "next-auth/providers/credentials"
+import { CredentialsProvider } from "next-auth/providers/credentials"
+import User from "@/models/user"
+import bcrypt from "bcryptjs"
+import dbConnect from "@/utils/dbConnect"
+
+export const authOptions = {
+    session: {
+        strategy: "jwt",
+    },
+    providers: [
+        CredentialsProvider({
+            async authorize(credentials, req) {
+                await dbConnect();
+            }
+        })
+    ]
+}
